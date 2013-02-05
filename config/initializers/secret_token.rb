157e322965f4ed71b2377a4149bfb052f5390e8c
Adam and Kyle <kyle+adam@appacademy.io>
2013-02-05 09:29:40 -0800

    empty rails project with gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cats::Application.config.secret_token = '45a754e2db3f4da677517fb6e435ea9d65b6b807ce81ccc0394e0db22c1cd2420f2caecede9030472d52a446491bd9fced4e20596cb6fad44f237f2db6dba968'
