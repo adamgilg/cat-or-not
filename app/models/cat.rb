de2473462fb036e595a82c079e335ec93b195441
Adam and Kyle <kyle+adam@appacademy.io>
2013-02-05 17:58:14 -0800

    Cats are ordered by average rating

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -6,6 +6,6 @@ class Cat < ActiveRecord::Base
   validates :picture, presence: true
 
   def self.ordered_by_rating
-    Cat.joins(:ratings).group(:cat_id).order('AVG(rating) DESC')
+    Cat.joins("LEFT OUTER JOIN ratings ON cats.id = ratings.cat_id").group(:cat_id).order('AVG(rating) DESC')
   end
 end
